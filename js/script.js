33ee922e4f6297378da384add950db61323904d0
coderRemo <155249320+coderRemo@users.noreply.github.com>
2024-07-29 21:57:45 +0530

    Initial Deployment

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Set current year
+const yearCopy = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearCopy.textContent = currentYear;
+
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("bg-dark");
+      navbar.classList.add("navbar-sticky");
+    } else {
+      navbar.classList.remove("bg-dark");
+      navbar.classList.remove("navbar-sticky");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", userScroll);
